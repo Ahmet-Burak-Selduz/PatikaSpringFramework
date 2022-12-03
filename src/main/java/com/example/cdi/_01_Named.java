e58324be0d9e442fe7f973f167531fcdb0cb077a
Ahmet Seldüz <burakkng.sldz@gmail.com>
2022-12-03 23:22:41 +0300

    @Named

---
+++ src/main/java/com/example/cdi/_01_Named.java
@@ -0,0 +1,21 @@
+package com.example.cdi;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Named;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+
+@Named(value = "namedtuto")
+@ApplicationScoped
+public class _01_Named {
+    private Long namedId;
+    private String namedData="Merhaba Cdi Named :)";
+}
